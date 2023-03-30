c5ae71f72e312ea4fd310172ec254ca07b8f0df5
isp426 <isp426@naver.com>
2023-03-30 16:34:07 +0900

    8393

---
+++ src/Backjun/bakjun8393.java
@@ -0,0 +1,25 @@
+package Backjun;
+import java.util.Scanner;
+public class bakjun8393 {
+
+
+			public static void main(String[] args) {
+					//문제1] 1~100까지 합을 구하는 프로그램 작성하기.
+				
+				int n = new Scanner(System.in).nextInt();
+				
+				int sum = 0;
+				
+				for (int i = 1; i <= n; i ++) {
+					sum += i;
+			
+				}System.out.println(sum);
+
+				
+
+			}
+
+		
+
+	}
+
